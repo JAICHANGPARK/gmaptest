e65dc8114fcc62978f17aaf164869fb59fadcd57
JAICHANGPARK <aristojeff@gmail.com>
2016-07-26 11:31:22 +0900

    지도에 선 그리기 추가
    폴리라인, 폴리곤, 원 그리기등 추가

--- gmaptest/ViewController.swift
+++ gmaptest/ViewController.swift
@@ -4,8 +4,11 @@
 //
 //  Created by PARK JAICHANG on 7/26/16.
 //  Copyright © 2016 JAICHANGPARK. All rights reserved.
-//
-// admob 이랑 gmsdk 사용 
+//  admob 과 google map sdk 
+// 수정 사항 
+// 1. polyline 
+// 2. polygon 
+// 3. circle 
 
 import UIKit
 import GoogleMaps
@@ -38,18 +41,28 @@ class ViewController: UIViewController, GADBannerViewDelegate, GMSMapViewDelegat
     
     let destinations = [VacationDestination(name: "sogang", location: CLLocationCoordinate2DMake(37.552333, 126.942374), zoom: 15),VacationDestination(name: "sogang", location: CLLocationCoordinate2DMake(37.549788, 126.913980), zoom: 15)]
     
+    /*-------------------------------------------
+        zoom : 
+     
+        1: World
+        5: Landmass/continent
+        10: City
+        15: Streets
+        20: Buildings
+    
+     ------------------------------------------*/
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         Request.testDevices = [kGADSimulatorID]
         bannerView.delegate = self
-        bannerView.adUnitID = "place your adunit"
+        bannerView.adUnitID = "place your adunit code"
         //bannerView.rootViewController = self
         bannerView.loadRequest(Request)
         
-        GMSServices.provideAPIKey("place your provide api key")
-        let camera = GMSCameraPosition.cameraWithLatitude(37.551549, longitude: 126.940774, zoom: 10)
+        GMSServices.provideAPIKey("place your google map sdk ios api key")
+        let camera = GMSCameraPosition.cameraWithLatitude(37.551549, longitude: 126.940774, zoom: 15)
         mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
        // mapView.myLocationEnabled = true
         self.view = mapView
@@ -76,6 +89,84 @@ class ViewController: UIViewController, GADBannerViewDelegate, GMSMapViewDelegat
         panoView.moveNearCoordinate(CLLocationCoordinate2D(latitude: -33.732, longitude: 150.312))
  
         */
+        // compass 추가
+        //let mapView = GMSMapView.mapWithFrame(.zero, camera: camera)
+        //mapView?.settings.compassButton = true
+        // my Location 버튼 추가
+        mapView?.settings.myLocationButton = true
+        // floor picker
+        mapView?.settings.indoorPicker = true
+        
+        //Polyline 사용 하여 경로 그리기
+        // 속성 
+        // 1. strokeWidth : 선 두께 결정
+        // 2. geodesic
+        // 3. strokeColor  = polyline.strokeColor = UIColor.blueColor()
+        //                   polyline.strokeColor = UIColor.redColor()
+        //
+        //
+        //
+        let path = GMSMutablePath()
+        path.addCoordinate(CLLocationCoordinate2D(latitude: 37.551549, longitude: 126.940774))
+        path.addCoordinate(CLLocationCoordinate2D(latitude: 37.552333, longitude: 126.942374))
+        path.addCoordinate(CLLocationCoordinate2D(latitude: 37.549788, longitude: 126.913980))
+        let rectangle = GMSPolyline(path: path)
+        rectangle.map = mapView
+        
+        rectangle.strokeWidth = 10.0
+        rectangle.strokeColor = UIColor.redColor()
+        
+        //---------------------------
+        
+        // polygon 
+        
+        // Create a rectangular path
+        let rect = GMSMutablePath()
+        rect.addCoordinate(CLLocationCoordinate2D(latitude: 37.551549, longitude: 126.940774))
+        rect.addCoordinate(CLLocationCoordinate2D(latitude: 37.552333, longitude: 126.942374))
+        rect.addCoordinate(CLLocationCoordinate2D(latitude: 37.549788, longitude: 126.913980))
+        //rect.addCoordinate(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.2))
+        
+        // Create the polygon, and assign it to the map.
+        let polygon = GMSPolygon(path: rect)
+        polygon.fillColor = UIColor(red: 0.25, green: 0, blue: 0, alpha: 0.05);
+        polygon.strokeColor = UIColor.blackColor()
+        polygon.strokeWidth = 2
+        polygon.map = mapView
+        
+        //----------------------------------polygon
+        
+        //---circle 
+        
+        let circleCenter = CLLocationCoordinate2D(latitude: 37.551549, longitude: 126.940774)
+        let circ = GMSCircle(position: circleCenter, radius: 1000)
+        
+        circ.fillColor = UIColor(red: 0.35, green: 0, blue: 0, alpha: 0.05)
+        circ.strokeColor = UIColor.redColor()
+        circ.strokeWidth = 5
+        circ.map = mapView
+        
+        //--------------------------------circle
+
+        //--Use GMSURLTileLayer to fetch tiles from URL
+        
+        let floor = 1
+        
+        // Implement GMSTileURLConstructor
+        // Returns a Tile based on the x,y,zoom coordinates, and the requested floor
+        let urls = { (x: UInt, y: UInt, zoom: UInt) -> NSURL in
+            let url = "https://www.example.com/floorplans/L\(floor)_\(zoom)_\(x)_\(y).png"
+            return NSURL(string: url)!
+        }
+        
+        // Create the GMSTileLayer
+        let layer = GMSURLTileLayer(URLConstructor: urls)
+        
+        // Display on the map at a specific zIndex
+        layer.zIndex = 100
+        layer.map = mapView
+        
+        //-----------------------Use GMSURLTileLayer to fetch tiles from URL
         
         let currentLocation = CLLocationCoordinate2DMake(37.551549, 126.940774)
         let marker = GMSMarker(position: currentLocation)
@@ -84,6 +175,7 @@ class ViewController: UIViewController, GADBannerViewDelegate, GMSMapViewDelegat
         
         // 뷰 상위에 버튼 생성 (코드로 만들어 버리는 경우 )
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .Plain, target: self, action: #selector(next))
+       
     }
     
         func next(){
@@ -123,8 +215,13 @@ class ViewController: UIViewController, GADBannerViewDelegate, GMSMapViewDelegat
         let marker = GMSMarker(position: currentDestination!.location)
         marker.title = currentDestination?.name
         marker.map = mapView
+        mapView?.settings.compassButton = true
+
+        
+        
     }
-}
+    
+    }
 
 
 
